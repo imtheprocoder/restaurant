6d9cab632341a09b604d7692fb4519c80f605126
Hassan Boss <Hassan_siala@hotmail.com>
2024-09-24 22:28:16 +0200

    WIP

--- frontend/src/pages/Home/HomePage.js
+++ frontend/src/pages/Home/HomePage.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect, useReducer } from 'react'
+import { getAll, search } from '../../services/foodService';
+import Thumbnails from '../../components/Thumbnails/Thumbnails';
+import { useParams } from 'react-router-dom';
+import Search from '../../components/Search/Search';
+
+
+const initialState = { foods: [] };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FOODS_LOADED':
+      return { ...state, foods: action.payload };
+    default:
+      return state;
+  }
+}
 
 export default function HomePage() {
+  
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { foods } = state;
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    const loadFoods = searchTerm ? search(searchTerm) : getAll();
+    
+    loadFoods.then(foods => 
+      dispatch({ type: 'FOODS_LOADED', payload: foods }));
+    }, [searchTerm]);
+  
   return (
-    <div>
-      
-    </div>
-  )
+  <>
+    <Search />
+    <Thumbnails foods={foods}/>
+  </>
+  );
 }
