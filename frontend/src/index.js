afe196367057e6eb9a988ea9778ce91b71939dd9
Hassan Boss <Hassan_siala@hotmail.com>
2024-09-29 09:09:25 +0200

    WIP

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import CartProvider from './hooks/useCart';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <BrowserRouter>
-  <App />
+    <BrowserRouter>
+      <CartProvider>
+        <App />
+      </CartProvider>
   </BrowserRouter>
-  
-  </React.StrictMode>
+</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ frontend/src/pages/Cart/CartPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './cartPage.module.css'
+import { useCart } from '../../hooks/useCart'
+
+export default function CartPage() {
+
+    const {cart} = useCart();
+
+  return <div>{cart.items.length}</div>
+  
+}
