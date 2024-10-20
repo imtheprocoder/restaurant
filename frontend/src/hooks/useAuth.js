d0b5dc4b8fd14c07e450e38b6e3f639a741fdf47
Hassan Boss <Hassan_siala@hotmail.com>
2024-10-20 02:38:07 +0200

    WIP

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -18,6 +18,17 @@ export const AuthProvider = ({ children }) => {
         }
     };
 
+    const register = async data => {
+        try{
+            const user = await userService.register(data);
+            setUser(user);
+            toast.success("Register Successful");
+        }
+        catch(err){
+            toast.error(err.response.data);
+        }
+    }
+
     const logout = () => {
         userService.logout();
         setUser(null);
@@ -25,7 +36,7 @@ export const AuthProvider = ({ children }) => {
     };
 
     return (
-        <AuthContext.Provider value={{ user, login, logout }}>
+        <AuthContext.Provider value={{ user, login, logout, register }}>
             {children}
         </AuthContext.Provider>
     );
